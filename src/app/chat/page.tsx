b05796ee62d57ea04ef0284d84ebfdfa542f1ddf
Kirill Leontiev <kirill.leontev@meduzzen.com>
2024-11-13 15:00:48 +0200

    feat: added loader to chat initial page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import { EmptyChat } from "@/components/Chat/EmptyChat";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { Loader } from "@/components/Loader/Loader";
 
 export default function ChatPage() {
+  const { loading } = useSelector((state: RootState) => state.chat);
   return (
     <div className="flex flex-col h-screen items-center justify-between bg-gray-100">
-      <EmptyChat/>
+      {loading ? (<Loader/>) : (<EmptyChat/>)}
     </div>
   );
 }
